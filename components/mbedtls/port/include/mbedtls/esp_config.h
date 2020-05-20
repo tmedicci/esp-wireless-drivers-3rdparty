bryan-hunt/esp-idf

Add cryptoauthlib as a component with mbedtls integration * Add options to mbedtls for hardware acceleration * Disable mbedtls EC curves if hardware acceleration is selected since the hardware will have to define the curves supported * Add a hardware ecdsa example

components/mbedtls/port/include/mbedtls/esp_config.h
@@ -310,6 +310,19 @@
 //#define MBEDTLS_AES_ENCRYPT_ALT
 //#define MBEDTLS_AES_DECRYPT_ALT
 
+#ifdef CONFIG_MBEDTLS_HARDWARE_ECDH
+#define MBEDTLS_ECDH_GEN_PUBLIC_ALT
+#define MBEDTLS_ECDH_COMPUTE_SHARED_ALT
+#endif
+
+#ifdef CONFIG_MBEDTLS_HARDWARE_ECDSA_SIGN
+#define MBEDTLS_ECDSA_SIGN_ALT
+#endif
+
+#ifdef CONFIG_MBEDTLS_HARDWARE_ECDSA_VERIFY
+#define MBEDTLS_ECDSA_VERIFY_ALT
+#endif
+
 /**
  * \def MBEDTLS_ENTROPY_HARDWARE_ALT
  *

tmedicci/esp-wireless-drivers-3rdparty

components/mbedtls/port/include/mbedtls/esp_config.h
@@ -149,6 +149,19 @@
 #undef MBEDTLS_MPI_MUL_MPI_ALT
 #endif
 
+#ifdef CONFIG_MBEDTLS_ATCA_HARDWARE_ECDH
+#define MBEDTLS_ECDH_GEN_PUBLIC_ALT
+#define MBEDTLS_ECDH_COMPUTE_SHARED_ALT
+#endif
+
+#ifdef CONFIG_MBEDTLS_ATCA_HARDWARE_ECDSA_SIGN
+#define MBEDTLS_ECDSA_SIGN_ALT
+#endif
+
+#ifdef CONFIG_MBEDTLS_ATCA_HARDWARE_ECDSA_VERIFY
+#define MBEDTLS_ECDSA_VERIFY_ALT
+#endif
+
 /**
  * \def MBEDTLS_ENTROPY_HARDWARE_ALT
  *