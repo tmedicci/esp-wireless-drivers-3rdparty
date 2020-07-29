9b7977be18e6bf89fa08e6dbf0dcb75b0b6a1a6d
morris <maoshengrong@espressif.com>
2020-07-29 13:13:51 +0800

    bringup esp32-s3 on FPGA

--- components/esp_wifi/esp32s3/esp_adapter.c
+++ components/esp_wifi/esp32s3/esp_adapter.c
@@ -1,9 +1,9 @@
-// Copyright 2019 Espressif Systems (Shanghai) PTE LTD
+// Copyright 2015-2020 Espressif Systems (Shanghai) PTE LTD
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -11,16 +11,3 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-#pragma once
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-#define TWAI_BRP_MIN                2
-#define TWAI_BRP_MAX                32768
-
-#ifdef __cplusplus
-}
-#endif
