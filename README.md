23446ec14809734424b6d1798cafa9badad10dde
Tiago Medicci Serrano <tiago.medicci@gmail.com>
2023-02-09 18:09:30 -0300

    Add prefixer utils to enable symbol redefinition

--- README.md
+++ README.md
@@ -5,3 +5,11 @@
 This project is used to integrate ESP32 family SoC's wireless software drivers into other platforms, like NuttX.
 
 Wireless software drivers mainly contains of hardware drivers, wireless protocols and utils.
+
+ctags --kinds-c=f esp-mbedtls/mbedtls/library/
+./prefixer.sh ./tags ../esp-mbedtls/mbedtls/
+ 3306  ./prefixer.sh ./tags ../wpa_supplicant/esp_supplicant
+ 3307  history prefixer
+ 3308  history "prefixer"
+ 3309  ./prefixer.sh ./tags ../esp-mbedtls/port
+ 3312  ./prefixer.sh ./tags ../wpa_supplicant/port/
---
+++ utils/prefixer.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+usage() {
+	echo "Usage: $0 ctags_file folder_to_find_and_replace"
+    exit 1
+}
+
+ctags_file="$1"; shift
+destination="$1"
+
+if [ -z "$ctags_file" ] || [ -z "$destination" ]; then
+    usage
+fi
+
+prefix="esp_"
+substitutions=0
+
+readtags_cmd="readtags -F '(list \$name #t)' -l ${ctags_file}"
+echo "readtags command is: ${readtags_cmd}"
+
+while read -r mbedtls_function ; do
+    find_cmd="grep -rl -e '[^${prefix}]${mbedtls_function}' ${destination}"
+    echo "Find command is: ${find_cmd}"
+    find_results="$(eval ${find_cmd})"
+
+    if [ -n "${find_results}" ]; then
+        find_results_n="$(echo "${find_results}" | wc -l)"
+        replace_cmd="echo \""${find_results}"\" | xargs sed -i \"s/\<${mbedtls_function}\>/${prefix}${mbedtls_function}/g\""
+        echo "Replace command is: ${replace_cmd}"
+        eval "${replace_cmd}"
+        replace_results=$?
+        substitutions=$((substitutions + find_results_n))
+        
+        if [ "${replace_results}" -ne '0' ]; then
+            echo "Failed to add \"${prefix}\" to \"${mbedtls_function}\""
+            echo "Aborting..."
+            exit 1
+        fi
+        echo "Added \"${prefix}\" prefix to \"${mbedtls_function}\" on the following occurences:"
+        echo "${find_results}"
+    # else
+        # echo "Error: \"${mbedtls_function}\" was not found on any file."
+        # echo "Aborting..."
+        # exit 1
+    fi
+
+done < <(eval "${readtags_cmd}" )
+
+echo "The prefix \"${prefix}\" was added to ${substitutions} functions"
