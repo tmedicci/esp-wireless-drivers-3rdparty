468c2a88a17760a3a30efd8765f556c4e1c17af0
Tiago Medicci Serrano <tiago.medicci@gmail.com>
2023-02-13 14:09:24 -0300

    Add esp-mbedtls folder to update_sources utils

--- utils/update_sources.sh
+++ utils/update_sources.sh
@@ -12,8 +12,18 @@ fi
 root_dir="$(git rev-parse --show-toplevel)"
 
 idf_version="$(git -C ${IDF_PATH} describe)"
-echo "Copying sources from local ESP-IDF ${idf_version}"
+echo "Copying sources from local ESP-IDF ${idf_version}."
 
-echo "Copying wpa_supplicant from ESP-IDF"
+echo "Copying wpa_supplicant from ESP-IDF..."
 rm -rf "${root_dir}/wpa_supplicant"
 cp -r "${IDF_PATH}/components/wpa_supplicant" "${root_dir}"
+
+echo "Copying mbedtls from ESP-IDF..."
+mbedtls_hash="$(git -C ${IDF_PATH}/components/mbedtls/mbedtls rev-parse HEAD)"
+echo "mbedTLS submodule: ${mbedtls_hash}"
+rm -rf "${root_dir}/esp-mbedtls"
+cp -r "${IDF_PATH}/components/mbedtls" "${root_dir}"
+mv "${root_dir}/mbedtls" "${root_dir}/esp-mbedtls"
+rm -rf "${root_dir}/esp-mbedtls/mbedtls"
+git submodule update --recursive --init
+git -C "${root_dir}/esp-mbedtls/mbedtls" checkout "${mbedtls_hash}"
