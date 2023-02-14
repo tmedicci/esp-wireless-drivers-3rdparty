5d68702616d4d9ae17f1b4639082a17621eccc5b
Tiago Medicci Serrano <tiago.medicci@gmail.com>
2023-02-14 11:04:45 -0300

    yUpdate update_sources to include header files and add path to make adjustments to them

--- utils/update_sources.sh
+++ utils/update_sources.sh
@@ -10,9 +10,16 @@ if [ "${result}" -ne '0' ]; then
 fi
 
 root_dir="$(git rev-parse --show-toplevel)"
+idf_tag="$(git -C ${IDF_PATH} describe)"
+idf_commit_hash="$(git -C ${IDF_PATH} rev-parse HEAD)"
 
-idf_version="$(git -C ${IDF_PATH} describe)"
-echo "Copying sources from local ESP-IDF ${idf_version}."
+echo "Copying sources from local ESP-IDF ${idf_tag}."
+echo "${idf_tag} commit ${idf_commit_hash}" > "${root_dir}/version"
+
+echo "Copying Wi-Fi library from ESP-IDF..."
+wifi_lib_hash="$(git -C ${IDF_PATH}/components/esp_wifi/lib rev-parse HEAD)"
+echo "ESP32 Wi-Fi lib submodule: ${wifi_lib_hash}"
+rm -rf "${root_dir}/libs/"
 
 echo "Copying wpa_supplicant from ESP-IDF..."
 rm -rf "${root_dir}/wpa_supplicant"
@@ -25,5 +32,29 @@ rm -rf "${root_dir}/esp-mbedtls"
 cp -r "${IDF_PATH}/components/mbedtls" "${root_dir}"
 mv "${root_dir}/mbedtls" "${root_dir}/esp-mbedtls"
 rm -rf "${root_dir}/esp-mbedtls/mbedtls"
-git submodule update --recursive --init
+
+echo "Copying include files from ESP-IDF..."
+cp -r "${IDF_PATH}/components/esp_wifi/include" "${root_dir}"
+cp -r "${IDF_PATH}/components/esp_phy/esp32s3/include/phy_init_data.h" "${root_dir}/include/esp32s3/phy_init_data.h"
+cp -r "${IDF_PATH}/components/esp_rom/include/esp32s3/rom/ets_sys.h" "${root_dir}/include/esp32s3/rom/ets_sys.h"
+cp -r "${IDF_PATH}/components/soc/esp32s3/include/soc/ledc_caps.h" "${root_dir}/include/esp32s3/soc/ledc_caps.h"
+cp -r "${IDF_PATH}/components/soc/esp32s3/include/soc/mpu_caps.h" "${root_dir}/include/esp32s3/soc/mpu_caps.h"
+cp -r "${IDF_PATH}/components/soc/esp32s3/include/soc/reg_base.h" "${root_dir}/include/esp32s3/soc/reg_base.h"
+cp -r "${IDF_PATH}/components/soc/esp32s3/include/soc/soc.h" "${root_dir}/include/esp32s3/soc/soc.h"
+cp -r "${IDF_PATH}/components/soc/esp32s3/include/soc/soc_caps.h" "${root_dir}/include/esp32s3/soc/soc_caps.h"
+cp -r "${IDF_PATH}/components/soc/esp32s3/include/soc/twai_caps.h" "${root_dir}/include/esp32s3/soc/twai_caps.h"
+cp -r "${IDF_PATH}/components/esp_common/include" "${root_dir}"
+cp -r "${IDF_PATH}/components/esp_hw_support/include/esp_cpu.h" "${root_dir}/include/esp_cpu.h"
+cp -r "${IDF_PATH}/components/esp_hw_support/include/esp_interface.h" "${root_dir}/include/esp_interface.h"
+cp -r "${IDF_PATH}/components/esp_hw_support/include/esp_mac.h" "${root_dir}/include/esp_mac.h"
+cp -r "${IDF_PATH}/components/esp_hw_support/include/esp_random.h" "${root_dir}/include/esp_random.h"
+cp -r "${IDF_PATH}/components/esp_event/include" "${root_dir}"
+cp -r "${IDF_PATH}/components/esp_phy/include" "${root_dir}"
+cp -r "${IDF_PATH}/components/esp_hw_support/include/esp_private" "${root_dir}/include"
+cp -r "${IDF_PATH}/components/esp_rom/include/esp_rom_md5.h" "${root_dir}/include/esp_rom_md5.h"
+cp -r "${IDF_PATH}/components/esp_system/include/esp_system.h" "${root_dir}/include/esp_system.h"
+cp -r "${IDF_PATH}/components/esp_timer/include/esp_timer.h" "${root_dir}/include/esp_timer.h"
+
+git submodule update --init --recursive
+git -C "${root_dir}/libs" checkout "${wifi_lib_hash}"
 git -C "${root_dir}/esp-mbedtls/mbedtls" checkout "${mbedtls_hash}"
